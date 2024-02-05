61c629fcb7b4b67203688f1dc957d8aea790808d
Steak <lakhotiya.sil07@gmail.com>
2024-02-05 09:20:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Body from './components/Body';
+import Head from './components/Head';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import Store from "./utils/Store";
+import { toggleMenu } from './utils/appSlice';
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import MainContainer from './components/MainContainer';
+import WatchPage from './components/WatchPage';
 function App() {
+
+const appRouter = createBrowserRouter([
+  {
+path: '/',
+element:<Body/>,
+children:[
+  {
+
+    path:"/",
+    element:<MainContainer/>
+  },
+  {
+    path:"/watch",
+    element:<WatchPage/>
+
+  }
+]
+  },
+  {
+
+  }
+])
+
   return (
-    <div className="App">
-    <h1>cdhsg</h1>
-    </div>
+<Provider store={Store}>
+      <div className="App">
+        <div style={{ position: 'sticky !important', top: "0", zIndex: "9999!important" }}>
+          <Head />
+        </div>
+
+        <div style={{ position: 'relative', zIndex: "0" }}>
+          <RouterProvider router={appRouter} />
+        </div>
+      </div>
+    </Provider>
+
+
+
   );
 }
 
 export default App;
+
+
+
+/*
+* Head 
+* Body
+* Sidebar
+*    -Menu Items
+* MainConatiner
+*    -ButtonsList
+*    -VideoContainer
+*       -VideoCard
+
+
+
+*/
